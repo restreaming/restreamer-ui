1b561bed51281944d355970e7ffa563faf7c26b7
KingIronMan2011 <176780813+KingIronMan2011@users.noreply.github.com>
2026-07-03 21:59:49 +0000

    fixes

---
+++ lingui.config.ts
@@ -0,0 +1,30 @@
+import { formatter } from '@lingui/format-po';
+
+export default {
+	catalogs: [
+		{
+			path: 'src/locales/{locale}/messages',
+			include: ['src/'],
+			exclude: ['**/node_modules/**'],
+		},
+	],
+	format: formatter({ lineNumbers: false }),
+	sourceLocale: 'en',
+	locales: [
+		'en',
+		'da',
+		'de',
+		'el',
+		'es',
+		'fr',
+		'it',
+		'ko',
+		'pl',
+		'pt-br',
+		'ru',
+		'sl',
+		'tr',
+		'uk',
+		'zh-hans',
+	],
+};
