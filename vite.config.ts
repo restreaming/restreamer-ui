5bde36aacba649b7c951e0358a441c886272c061
KingIronMan2011 <176780813+KingIronMan2011@users.noreply.github.com>
2026-07-24 00:45:17 +0000

    refactor: update dependencies and configuration for SWC support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import { linguiMacroSwcPlugin } from '@lingui/swc-plugin/options';
 import { vitePages } from '@kingironman2011/vite-pages';
 
 export default defineConfig({
 	base: '/',
-	plugins: [react(), vitePages()],
+	plugins: [
+		react({
+			plugins: [linguiMacroSwcPlugin()],
+		}),
+		vitePages(),
+	],
 	server: {
 		host: true,
 		port: 3000,
